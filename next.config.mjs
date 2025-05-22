728b92f92c8d09837c075a6793389a766ecf8c0b
rahulrajdahal <rahulrajdahal@gmail.com>
2025-05-22 21:33:57 +0545

    fix: next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [new URL("https://res.cloudinary.com/**")],
+  },
+};
 
 export default nextConfig;
