00909e02d2e1ece506ac336e1eb5f231cfc05d88
rahulrajdahal <rahulrajdahal@gmail.com>
2025-06-15 22:54:11 +0545

    fix: push notificaitons toast

--- app/Books.tsx
+++ app/Books.tsx
@@ -1,14 +1,25 @@
 "use client";
 
 import { Card, PageLayout } from "@/components";
+import { useIsPushNotification } from "@/hooks";
 import { BookWithAuthorAndGenre } from "@/types";
 import { routes } from "@/utils/routes";
+import { useEffect } from "react";
+import toast from "react-hot-toast";
 
 type BooksProps = {
   books: BookWithAuthorAndGenre[];
 };
 
 export default function Books({ books }: Readonly<BooksProps>) {
+  const { isSupported } = useIsPushNotification();
+
+  useEffect(() => {
+    if (!isSupported) {
+      toast.error("Push notifications not supported in this browser.");
+    }
+  }, []);
+
   return (
     <PageLayout className="mt-10 grid grid-cols-1 gap-8 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
       {books.map((book) => (
