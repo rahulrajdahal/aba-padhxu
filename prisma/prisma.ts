aad613cdc38726f72429ee3768b1e9d9c0917336
Rahul Raj Dahal <rahulrajdahal@gmail.com>
2026-06-14 19:27:38 +0100

    fix: updagraded prisma orm and added config for udpate

--- prisma/prisma.ts
+++ prisma/prisma.ts
@@ -1,11 +1,10 @@
-import 'server-only';
+import { PrismaClient } from "@/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import "dotenv/config";
 
-import { PrismaClient } from "@prisma/client";
+const connectionString = `${process.env.DATABASE_URL}`;
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({ adapter });
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
-
-export default prisma;
+export { prisma };
